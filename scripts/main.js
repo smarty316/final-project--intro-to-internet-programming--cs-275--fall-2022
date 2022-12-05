d64518868e110b3297a433cfb22772e52da82035
smarty316 <jlshefsky@gmail.com>
2022-12-05 12:15:11 -0500

    Modal added

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+let modal_btn = document.createElement(`button`);
+modal_button.appendchild(modal_btn);
+document.getElementById(`modal_button`).addEventListener(`click`,() => {
+    `modal-panel`.style.display = `block`;
+})
